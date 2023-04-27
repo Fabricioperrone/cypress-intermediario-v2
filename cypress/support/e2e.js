354919eb47370931d2f858b8b4570f91a5bf4dc9
Fabricio Acer <fabricio_luis@outlook.com>
2023-04-27 13:29:35 -0300

    update-curso-cypress

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,18 @@
+import 'cypress-plugin-api'
+
 import './api_commands'
+import './cli_commands'
 import './gui_commands'
+
+
+  // Hide fetch/XHR requests
+  const app = window.top;
+  if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+    const style = app.document.createElement('style');
+    style.innerHTML =
+      '.command-name-request, .command-name-xhr { display: none }';
+    style.setAttribute('data-hide-command-log-request', '');
+  
+    app.document.head.appendChild(style);
+  }
+  
